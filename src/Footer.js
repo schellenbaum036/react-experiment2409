8b85c99dd0983ca17baa50650ea559414ea80090
鈴木　理 <schellenbaum-036@hotmail.co.jp>
2024-09-13 13:17:33 +0900

    *

--- src/Footer.js
+++ src/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <header>
+    <footer>
       <nav>
         <a href="#top">トップ</a>
         <a href="#daily-report">日報</a>
         <a href="#illustration">図解</a>
       </nav>
-    </header>
+    </footer>
   );
 }
 
